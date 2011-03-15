bb0851f1ee362ff0453119e7319b2bbd5116f130
flop25 <flop25@piwigo.org>
2011-03-15 20:29:49 +0000

    Massive update ! sprite for icons old icons deleted adding a text for about.php version changed to auto for all themes a few other changes
    git-svn-id: http://piwigo.org/svn/extensions/floPure/Pure_tr_green_nature@9716 68402e56-0260-453c-a942-63ccdbb3a9ee

--- themeconf.inc.php
+++ themeconf.inc.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Theme Name: Pure_tr_green_nature
-Version: 1.0.0
+Version: auto
 Description: Rounded corners and a green inspired from the nature, these are the two characteristics of this style.
 Theme URI: http://fr.piwigo.org/ext/extension_view.php?eid=442
 Author: flop25
-Author URI: http://www.planete-flop.fr/site.php?acc=file
+Author URI: http://www.planete-flop.fr/
 */
 
 $themeconf = array(
